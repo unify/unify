56f8616f68c0cfda36325dc0a3e7f13313b4b297
Sebastian Werner <info@sebastian-werner.net>
2010-09-23 22:13:13 +0200

    Fixed some issues with comments

--- framework/source/class/unify/event/__init__.js
+++ framework/source/class/unify/event/__init__.js
@@ -1,12 +1,3 @@
-/* ***********************************************************************************************
-
-    Unify Project
-    
-    Homepage: unify-project.org
-    License: MIT + Apache (V2)
-    Copyright: 2009-2010 Deutsche Telekom AG, Germany, http://telekom.com
-
-*********************************************************************************************** */
 /** 
  * Additional event managers and event types which are currently
  * missing in qooxdoo.
--- framework/source/class/unify/event/handler/__init__.js
+++ framework/source/class/unify/event/handler/__init__.js
@@ -1,12 +1,3 @@
-/* ***********************************************************************************************
-
-    Unify Project
-    
-    Homepage: unify-project.org
-    License: MIT + Apache (V2)
-    Copyright: 2009-2010 Deutsche Telekom AG, Germany, http://telekom.com
-
-*********************************************************************************************** */
 /** 
  * Additional event handlers not found in qooxdoo. Often for specific features of HTML5 or mobile devices.
  */
