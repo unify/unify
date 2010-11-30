d8aaac90a96acfac29cc64b28f06a77bd6acd54f
Sebastian Werner <se.werner@telekom.de>
2010-11-30 17:48:03 +0100

    Increased photo count

--- application/flicky/source/class/flicky/business/Flickr.js
+++ application/flicky/source/class/flicky/business/Flickr.js
@@ -12,8 +12,8 @@ qx.Class.define("flicky.business.Flickr",
     
     this.setEnableProxy(false);
     
-    this._addService("recent", {url:"http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20flickr.photos.recent(16)&format=json&diagnostics=true", keep: 60}); // 1 minute
-    this._addService("interesting", {url:"http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20flickr.photos.interestingness(16)&format=json&diagnostics=true", keep: 60*60*24}); // 1 day
+    this._addService("recent", {url:"http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20flickr.photos.recent(32)&format=json&diagnostics=true", keep: 60}); // 1 minute
+    this._addService("interesting", {url:"http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20flickr.photos.interestingness(32)&format=json&diagnostics=true", keep: 60*60*24}); // 1 day
     this._addService("info", {url:"http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20flickr.photos.info%20where%20photo_id%3D'%photo%'&format=json&diagnostics=true", keep: 60*60*24*14}) // 14 days
   }
 });
