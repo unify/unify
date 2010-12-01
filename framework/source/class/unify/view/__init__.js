2eba10a69e583569da596680e5a15c60f864c34a
Sebastian Werner <se.werner@telekom.de>
2010-12-01 15:57:52 +0100

    Moved classes one level up

--- framework/source/class/unify/view/__init__.js
+++ framework/source/class/unify/view/__init__.js
@@ -1,4 +1,6 @@
 /**
- * View controller for Unify applications.
+ * Typical smartphone view handling. Includes features to connect to unify 
+ * business objects. Automatic structural managment with support for typical 
+ * iPhone-style navigation.
  * 
  */
