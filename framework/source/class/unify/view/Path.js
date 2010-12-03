54ff588148bf1744f49c3e64ce4f01fcc5f46110
Sebastian Werner <se.werner@telekom.de>
2010-12-03 14:23:47 +0100

    Made class from Path to use as an alternative to the previously used Array but with same performance characterism.

--- framework/source/class/unify/view/Path.js
+++ framework/source/class/unify/view/Path.js
@@ -1,12 +1,59 @@
 qx.Class.define("unify.view.Path",
 {
+  extend : qx.type.BaseArray,
+  
+  members : 
+  {
+    __modified : false,
+    
+    clone : function()
+    {
+      var clone = new unify.view.Path;
+      var entry;
+      for (var i=0, l=this.length; i<l; i++) 
+      {
+        entry = this[i];
+        clone.push({
+          view : entry.view,
+          segment : entry.segment,
+          param : entry.param
+        });
+      }
+      
+      return clone;
+    },
+    
+    setSegment : function(segment, pos, clone)
+    {
+      
+    }
+    
+    
+  },
+  
   statics :
   {
-    __urlMatcher : /^([a-z-]+)(\.([a-z-]+))?(\:([a-zA-Z0-9_-]+))?$/,
+    fromString : function(str)
+    {
+      var obj = new unify.view.Path;
+      
+      if (str.length > 0) 
+      {
+        var fragments = str.split("/");
+        for (var i=0, l=fragments.length; i<l; i++) {
+          obj.push(this.parseFragment(fragments[i]));
+        }
+      }
+      
+      return obj;
+    },
+    
+    
+    __fragmentMatcher : /^([a-z-]+)?(\.([a-z-]+))?(\:([a-zA-Z0-9_-]+))?$/,
     
     parseFragment : function(fragment)
     {
-      var match = this.__urlMatcher.exec(fragment);
+      this.__fragmentMatcher.exec(fragment);
       return {
         view : RegExp.$1 || null,
         segment : RegExp.$3 || null,
