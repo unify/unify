aa11bf789c593cca6281a14801cefdf69acd6dfb
Sebastian Werner <se.werner@telekom.de>
2010-12-08 15:47:03 +0100

    Added new test application: feedreader

---
+++ application/feedreader/source/class/feedreader/Application.js
@@ -0,0 +1,52 @@
+/* ************************************************************************
+
+   feedreader
+
+   Copyright:
+     2010 Deutsche Telekom AG, Germany, http://telekom.com
+
+ ************************************************************************ */
+
+/* ************************************************************************
+
+#asset(feedreader/*)
+
+************************************************************************ */
+
+/**
+ * Unify application class
+ */
+qx.Class.define("feedreader.Application", 
+{
+  extend : unify.Application,
+
+  members : 
+  {
+    // overridden
+    main : function() 
+    {
+      // Call super class
+      this.base(arguments);
+
+      // Configure application
+      document.title = "feedreader";
+
+      // Master view
+      var MasterViewManager = new unify.view.ViewManager("master");
+      MasterViewManager.add(feedreader.view.Start);
+      
+      
+      // Configure tab view
+      var TabView = new unify.view.TabView;
+      TabView.add(MasterViewManager);
+      this.add(TabView);
+      
+
+      
+      // Add at least one view manager to the navigation managment
+      var Navigation = unify.view.Navigation.getInstance();
+      Navigation.add(MasterViewManager);
+      Navigation.init();      
+    }
+  }
+});
---
+++ application/feedreader/source/class/feedreader/view/Start.js
@@ -0,0 +1,39 @@
+/* ************************************************************************
+
+  feedreader
+
+  Copyright:
+    2009 Deutsche Telekom AG, Germany, http://telekom.com
+
+ ************************************************************************ */
+
+/**
+ * Start View
+ */
+qx.Class.define("feedreader.view.Start", {
+  extend : unify.view.StaticView,
+  type : "singleton",
+
+  members : 
+  {
+    // overridden
+    getTitle : function(type, param) {
+      return "Start";
+    },
+
+    
+    // overridden
+    _createView : function() 
+    {
+      var layer = new unify.ui.Layer(this);
+      var titlebar = new unify.ui.TitleBar(this);
+      layer.add(titlebar);
+      
+      var content = new unify.ui.Content;
+      content.add("Hello World");
+      layer.add(content);
+
+      return layer;
+    }
+  }
+});
