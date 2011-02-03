f06658d34da703678dc24ecc5c901951b804e6e8
Sebastian Werner <se.werner@telekom.de>
2011-02-03 17:15:56 +0100

    Minor fixes

--- tool/release.sh
+++ tool/release.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-root=`dirname $0`
+root=`dirname $0/`/..
 echo ">>> Path: $root"
 cd $root || exit 1
-version=`cat ../version.txt`
+version=`cat version.txt`
 
 echo ">>> Version: $version"
 echo ">>> If there is an error tagging the project you might to increment the version counter"
