f6596240ba68ce13c08254cb864f0f353320e313
Sebastian Fastner <mail@sebastianfastner.de>
2011-03-04 16:22:15 +0100

    Add scroll area to test application

--- application/widgets/source/class/widgets/view/Start.js
+++ application/widgets/source/class/widgets/view/Start.js
@@ -35,12 +35,32 @@ qx.Class.define("widgets.view.Start", {
       layer.add(content);*/
       
       var layerWidget = new unify.ui.widget.core.Layer(layer);
-      var cont = new unify.ui.widget.basic.Label("Das ist ein Test");
+      /*var cont = new unify.ui.widget.basic.Label("Das ist ein Test");
       layerWidget.add(cont, {
         left: 50,
         top: 10
       });
       cont.set({
+        width: 100,
+        height: 50
+      });*/
+
+      var scroller = new unify.ui.widget.container.Scroll();
+      layerWidget.add(scroller, {
+        left: 50,
+        top: 50
+      });
+      scroller.set({
+        width: 300,
+        height: 300
+      });
+
+      var label = new unify.ui.widget.basic.Label("Das ist ein Test");
+      scroller.add(label, {
+        left: 50,
+        top: 10
+      });
+      label.set({
         width: 100,
         height: 50
       });
