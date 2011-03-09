898a473a12f1f721c5a4ec8ea17c9d6270792bfe
Sebastian Fastner <mail@sebastianfastner.de>
2011-03-09 16:48:14 +0100

    Add new container components

---
+++ framework/source/class/unify/ui/widget/container/NavigationBar.js
@@ -0,0 +1,11 @@
+qx.Class.define("unify.ui.widget.container.NavigationBar", {
+  extend: unify.ui.widget.container.Composite,
+  
+  construct : function() {
+    this.base(arguments, new qx.ui.layout.HBox());
+  },
+  
+  members : {
+    
+  }
+});
