f2d473703e5000d86193f37eb8bfc04c693d6059
Sebastian Fastner <mail@sebastianfastner.de>
2011-03-13 10:42:57 +0100

    Widget implementation of NavigationBar

--- framework/source/class/unify/ui/widget/container/NavigationBar.js
+++ framework/source/class/unify/ui/widget/container/NavigationBar.js
@@ -1,11 +1,48 @@
 qx.Class.define("unify.ui.widget.container.NavigationBar", {
   extend: unify.ui.widget.container.Composite,
   
-  construct : function() {
-    this.base(arguments, new qx.ui.layout.HBox());
+  construct : function(view) {
+    this.base(arguments, new qx.ui.layout.Basic());
+    
+    this.__view = view;
+  },
+  
+  properties : {
+    minWidth: {
+      refine: true,
+      init: 200
+    },
+    height: {
+      refine: true,
+      init: 42
+    }
   },
   
   members : {
+    __view : null,
+    __navigationBar : null,
+  
+    _createElement : function() {
+      var navBar = this.__navigationBar = new unify.ui.NavigationBar(this.__view);
+      return navBar.getElement();
+    },
     
+    setItems : function(items) {
+      this.__navigationBar.setItems(items);
+    },
+    
+    getItems : function() {
+      return this.__navigationBar.getItems();
+    },
+    
+    resetItems : function() {
+      this.__navigationBar.resetItems();
+    }
+  },
+  
+  destruct : function() {
+    this.__view = null;
+    this.__navigationBar.dispose();
+    this.__navigationBar = null;
   }
 });
