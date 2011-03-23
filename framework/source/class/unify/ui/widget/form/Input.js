0a1f7c660408a0695290854b08cbe9678aae6a87
Sebastian Fastner <mail@sebastianfastner.de>
2011-03-24 00:15:54 +0100

    Change input behaviour to use bom input module

--- framework/source/class/unify/ui/widget/form/Input.js
+++ framework/source/class/unify/ui/widget/form/Input.js
@@ -1,2 +1,48 @@
+/* ***********************************************************************************************
+
+    Unify Project
+
+    Homepage: unify-project.org
+    License: MIT + Apache (V2)
+    Copyright: 2010, Sebastian Fastner, Mainz, Germany, http://unify-training.com
+
+*********************************************************************************************** */
+
+/*
+#require(qx.event.handler.Input)
+*/
+
+/**
+ * Input component
+ */
 qx.Class.define("unify.ui.widget.form.Input", {
+  extend : unify.ui.widget.core.Widget,
+  
+  properties : {
+    columns : {
+      init: 3
+    },
+    rows : {
+      init: 3
+    }
+  },
+  
+  members : {
+    _createElement : function() {
+      var e = qx.bom.Input.create("textarea", {
+        rows: this.getRows(),
+        cols: this.getColumns()
+      });
+      //qx.event.Registration.addListener(e, "input", this._onChange, this);
+      return e;
+    },
+    
+    setValue : function(value) {
+      qx.bom.Input.setValue(this.getElement(), value);
+    },
+    
+    getValue : function() {
+      return qx.bom.Input.getValue(this.getElement());
+    }
+  }
 });
