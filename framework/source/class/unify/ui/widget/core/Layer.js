1f5cfe59f645c0cbf9cb7448a526c0e9544c8fd3
Sebastian Fastner <mail@sebastianfastner.de>
2011-04-01 20:11:20 +0200

    Add resize handler to start new layouting process

--- framework/source/class/unify/ui/widget/core/Layer.js
+++ framework/source/class/unify/ui/widget/core/Layer.js
@@ -37,11 +37,13 @@ qx.Class.define("unify.ui.widget.core.Layer", {
     qx.bom.element2.Style.set(elem, "boxSizing", "border-box")
     
     view.addListener("appear", this.__viewAppear, this);
+    qx.event.Registration.addListener(window, "resize", this.__onResize, this);
   },
   
   members: {
     __elem : null,
     __layer : null,
+    __dirty : true,
     
     getUILayer: function() {
       return this.__layer;
@@ -62,17 +64,24 @@ qx.Class.define("unify.ui.widget.core.Layer", {
       this.renderChildren();
     },
     
+    __onResize : function(e) {
+      qx.ui.core.queue.Layout.add(this);
+    },
+    
+    renderLayout : function(left, top, width, height) {
+      this.base(arguments, left, top, width, height, true);
+    },
+    
     /** Returns fixed size hint of base layer size */
     getSizeHint : function() {
       var Dimension = qx.bom.element2.Dimension;
       
       var e = this.getElement();
-
       var ret = {
         width: Dimension.getContentWidth(e),
         height: Dimension.getContentHeight(e)
       };
-      
+
       return ret;
     }
   },
