cc3dcb834586906d89d5c7b6e94318d93413e2fc
Sebastian Fastner <mail@sebastianfastner.de>
2011-04-14 17:49:45 +0200

    Enable and disable of child widgets support

--- framework/source/class/unify/ui/widget/container/ToolBar.js
+++ framework/source/class/unify/ui/widget/container/ToolBar.js
@@ -6,6 +6,12 @@ qx.Class.define("unify.ui.widget.container.ToolBar", {
   },
   
   members : {
+    /*
+    ---------------------------------------------------------------------------
+      PUBLIC API
+    ---------------------------------------------------------------------------
+    */
+    
     setItems : function(items) {
       var elem = this.getElement();
       var itemElem;
@@ -16,6 +22,30 @@ qx.Class.define("unify.ui.widget.container.ToolBar", {
         elem._add(itemElem);
       }
     },
+    
+    /**
+     * disables all items of this Toolbar
+     */
+    disable: function(){
+      if (this._hasChildren) {
+        var children = this._getChildren();
+        for (var i=0,ii=children.length; i<ii; i++) {
+          children[i].setEnabled(false);
+        }
+      }
+    },
+    
+    /**
+     * enables all items of this Toolbar
+     */
+    enable: function(){
+      if (this._hasChildren) {
+        var children = this._getChildren();
+        for (var i=0,ii=children.length; i<ii; i++) {
+          children[i].setEnabled(true);
+        }
+      }
+    },
 
 
     /*
@@ -24,67 +54,9 @@ qx.Class.define("unify.ui.widget.container.ToolBar", {
     ---------------------------------------------------------------------------
     */
     
-/*    __onChangeSegment : function(e)
-    {
-      var 
-        Class = qx.bom.element2.Class,
-        allSegmented = e.getTarget().getElement().querySelectorAll(".segmented");
-        
-      for (var i = 0, l =  allSegmented.length; i < l; i += 1) 
-      {
-        if (allSegmented[i])
-        {
-          var old = allSegmented[i].querySelector(".selected");
-          if (old) 
-          {
-            Class.remove(old, "selected");
-          }
-
-          var next = allSegmented[i].querySelector("[goto='." + e.getData() + "']");
-          if (next) 
-          {
-            Class.add(next, "selected");
-          }
-        }
-      }
-      
-    },
-    
-    
-    __createSegmentButtonElement : function(config, selected)
-    {
-      var buttonElem = document.createElement("div");
-      buttonElem.setAttribute("goto", "." + config.segment);
-      buttonElem.className = config.segment == selected ? "button selected" : "button";
-      
-      if (config.label) {
-        buttonElem.innerHTML = config.label;
-      } else if (config.icon) {
-        buttonElem.innerHTML = "<div/>";
-      }
-      
-      return buttonElem;
-    },*/
-
     _createItemElement : function(config)
     {
       var itemElem = new unify.ui.widget.form.Button();
-      /*
-      // create base element
-      itemElem = document.createElement(config.kind == "header" ? "h1" : "div");
-      
-      // special segment handling
-      if (config.kind == "segmented")
-      {
-        var view = config.view;
-        var segment = view.getSegment();
-        
-        var buttons = config.buttons;
-        for (var i=0, l=buttons.length; i<l; i++) {
-          itemElem.appendChild(this.__createSegmentButtonElement(buttons[i], segment));
-        }
-        view.addListener("changeSegment", this.__onChangeSegment, this);
-      }*/
 
       var navigation = {};
       
@@ -101,32 +73,6 @@ qx.Class.define("unify.ui.widget.container.ToolBar", {
       } else if (config.show) {
         navigation.show = config.show;
       }
-/*      
-      // spacer sizing
-      if (config.size) {
-        itemElem.setAttribute("style", 'max-width: ' + config.size + '; min-width: ' + config.size + ';');
-      }
-      
-      // add kind as CSS class
-      if (config.kind) {
-        qx.bom.element2.Class.add(itemElem, config.kind);
-      }
-      
-      // add additional classes
-      if (config.addclass) {
-        qx.bom.element2.Class.add(itemElem, config.addclass);
-      }
-      
-      if (config.style) {
-        qx.bom.element2.Class.add(itemElem, config.style);
-      }
-      
-      // add label or icon container
-      if (config.label) {
-        itemElem.innerHTML = config.label;
-      } else if (config.icon) {
-        itemElem.innerHTML = "<div/>";
-      }*/
 
       if (config.label) {
         itemElem.setValue(config.label);
