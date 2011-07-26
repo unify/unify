15b0b1e6d9f7c1fa5330100a4aebfefc04bb9d46
Sebastian Fastner <mail@sebastianfastner.de>
2011-07-26 18:38:05 +0200

    Add list widget

--- unify/framework/source/class/unify/ui/widget/container/List.js
+++ unify/framework/source/class/unify/ui/widget/container/List.js
@@ -4,10 +4,19 @@ qx.Class.define("unify.ui.widget.container.List", {
   construct : function() {
     this.base(arguments);
     
-    this._setLayout(new qx.ui.layout.VBox());
+    var layout = new qx.ui.layout.Grid();
+    layout.setColumnFlex(1, 1);
+    this._setLayout(layout);
   },
   
   properties : {
+    // overridden
+    appearance :
+    {
+      refine: true,
+      init: "list"
+    },
+    
     data : {
       apply: "_applyData"
     }
@@ -17,13 +26,31 @@ qx.Class.define("unify.ui.widget.container.List", {
     _applyData : function(data) {
       this._removeAll();
       
+      var rowCounter = 0;
+      
       var header, fields, title;
       for (header in data) {
-        this._add(new unify.ui.widget.basic.Label(header));
+        this._add(
+          new unify.ui.widget.basic.Label(header).set({
+            appearance: "list.header"
+          }),
+          { row: rowCounter++, column: 0, colSpan: 2 }
+        );
         
         fields = data[header];
         for (title in fields) {
-          this._add(new unify.ui.widget.basic.Label(title + ": " + fields[title]));
+          this._add(
+            new unify.ui.widget.basic.Label(title).set({
+              appearance: "list.description"
+            }),
+            { row: rowCounter, column: 0 }
+          );
+          this._add(
+            new unify.ui.widget.basic.Label(fields[title]).set({
+              appearance: "list.value"
+            }),
+            { row: rowCounter++, column: 1 }
+          );
         }
       }
     }
