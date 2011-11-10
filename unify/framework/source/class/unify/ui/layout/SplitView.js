db68da46a5e4f8825a320422742dbfda5fb0fe2d
Sebastian Fastner <mail@sebastianfastner.de>
2011-11-10 13:50:48 +0100

    Add configurable split view

--- unify/framework/source/class/unify/ui/layout/SplitView.js
+++ unify/framework/source/class/unify/ui/layout/SplitView.js
@@ -14,8 +14,18 @@
 qx.Class.define("unify.ui.layout.SplitView", {
   extend : qx.ui.layout.Abstract,
   
-  statics : {
-    SPACER : 6
+  construct : function(splitLevel) {
+    this.base(arguments);
+    
+    if (splitLevel) {
+      this.setSplitLevel(splitLevel);
+    }
+  },
+  
+  properties : {
+    splitLevel : {
+      init: 0.33
+    }
   },
   
   members : {
@@ -30,12 +40,19 @@ qx.Class.define("unify.ui.layout.SplitView", {
         this.error("SplitView supports only exactly 2 children!");
       }
       
+      var masterViewManager = children[0].getUserData("viewManager");
       if (availWidth < availHeight) {
         children[1].renderLayout(0, 0, availWidth, availHeight);
+        if (masterViewManager) {
+          masterViewManager.setDisplayMode("popover");
+        }
       } else {
-        var leftWidth = Math.round(availWidth * 0.33);
+        var leftWidth = Math.round(availWidth * this.getSplitLevel());
         children[0].renderLayout(0, 0, leftWidth, availHeight);
         children[1].renderLayout(leftWidth, 0, availWidth-leftWidth, availHeight);
+        if (masterViewManager) {
+          masterViewManager.resetDisplayMode();
+        }
       }
     }
   }
