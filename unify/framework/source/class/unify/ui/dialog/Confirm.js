9b57d813089ed218b8dd6ef0fb45b3199ac14a44
Zhang Ming <m.zhang@external.telekom.de>
2011-12-13 10:46:18 +0100

    added basic dialog widgets alert and confirm

---
+++ unify/framework/source/class/unify/ui/dialog/Confirm.js
@@ -0,0 +1,23 @@
+/* ***********************************************************************************************
+
+    Unify Project
+
+    Homepage: unify-project.org
+    License: MIT + Apache (V2)
+    Copyright: 2011 Deutsche Telekom AG, Germany, http://telekom.com
+    
+*********************************************************************************************** */
+
+/**
+ * EXPERIMENTAL
+ */
+qx.Class.define("unify.ui.dialog.Confirm", {
+  extend: unify.ui.dialog.Alert,
+  
+  construct : function(value, onOk, onCancel) {
+    this.base(arguments, value, onOk);
+    //call parent function to add a "cancel" button and his callback function.
+    this.__addButton("Cancel", onCancel);
+  }
+  
+});
