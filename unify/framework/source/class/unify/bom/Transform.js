e19d67b55a883f51d1ad49973ed6b81cf56103a0
Dominik Göpel <dominik.goepel@gmx.de>
2011-12-29 16:37:14 +0100

    added accelScale and scale to unify.bom.Transform

--- unify/framework/source/class/unify/bom/Transform.js
+++ unify/framework/source/class/unify/bom/Transform.js
@@ -26,6 +26,19 @@ qx.Class.define("unify.bom.Transform",
     
     translate : function(x, y) {
       return "translate("+x+","+y+")";
+    },
+
+    accelScale : function(x,y){
+      // Fix resize bug with translate3d on google chrome 15.0.874.121 m
+      if (qx.core.Environment.get("browser.name") == "chrome" || qx.core.Environment.get("engine.name") != "webkit") {
+        return "scale("+x+","+y+")";
+      } else {
+        return "scale3d("+x+","+y+",1)";
+      }
+    },
+
+    scale : function (x,y){
+      return "scale("+x+","+y+")";
     }
   }
 });
