8761a0e3a8ea7f22600c361a9bca51f6acf7f4ea
Dominik Göpel <dominik.goepel@gmx.de>
2011-12-29 16:45:28 +0100

    added cancel to AnimationFrame and updated polyfill of requestAnimationFrame

--- unify/framework/source/class/unify/fx/core/AnimationFrame.js
+++ unify/framework/source/class/unify/fx/core/AnimationFrame.js
@@ -12,21 +12,32 @@
  * Use external requestAnimationFrame patches from zynga
  */
 /*
-#ignore(requestAnimationFrame)
+ #ignore(requestAnimationFrame)
+ #ignore(cancelAnimationFrame)
  */
 
 qx.Class.define("unify.fx.core.AnimationFrame", {
   type: "static",
-  
+
   statics: {
     /**
      * Request animation frame
      *
      * @param callback {Function} Callback function if animation frame is hitten
-     * @param root {Object?null} Root node
+     * @param root {Object?} optional html element defining the area where the animation happens
+     * @return {Object} request handle
      */
-    request : function(callback, root) {
-      requestAnimationFrame(callback, root);
+    request : function(callback,root) {
+      return requestAnimationFrame(callback,root);//TODO check spec if root element is still there and if browsers support it
+    },
+
+    /**
+     * cancels a pending animation frame step
+     * 
+     * @param handle {Object} the pending requests handle
+     */
+    cancel : function(handle) {
+      return cancelAnimationFrame(handle);
     }
   }
 });
@@ -55,106 +66,90 @@ qx.Class.define("unify.fx.core.AnimationFrame", {
  */
 (function(global)
 {
-	if(global.requestAnimationFrame) {
-		return;
-	}
-
-	// Basic emulation of native methods for internal use
-
-	var now = Date.now || function() {
-		return +new Date;
-	};
-
-	var getKeys = Object.keys || function(obj) {
-
-		var keys = {};
-		for (var key in obj) {
-			keys[key] = true;
-		}
-
-		return keys;
-
-	};
-
-	var isEmpty = Object.empty || function(obj) {
-
-		for (var key in obj) {
-			return false;
-		}
-
-		return true;
-
-	};
-
-
-	// requestAnimationFrame polyfill
-	// http://webstuff.nfshost.com/anim-timing/Overview.html
-
-	var postfix = "RequestAnimationFrame";
-	var prefix = (function()
-	{
-		var all = "webkit,moz,o,ms".split(",");
-		for (var i=0; i<4; i++) {
-			if (global[all[i]+postfix] != null) {
-				return all[i];
-			}
-		}
-	})();
-
-	// Vendor specific implementation
-	if (prefix)
-	{
-		global.requestAnimationFrame = global[prefix+postfix];
-		global.cancelRequestAnimationFrame = global[prefix+"Cancel"+postfix];
-		return;
-	}
-
-	// Custom implementation
-	var TARGET_FPS = 60;
-	var requests = {};
-	var rafHandle = 1;
-	var timeoutHandle = null;
-
-	global.requestAnimationFrame = function(callback, root)
-	{
-		var callbackHandle = rafHandle++;
-
-		// Store callback
-		requests[callbackHandle] = callback;
-
-		// Create timeout at first request
-		if (timeoutHandle === null)
-		{
-			timeoutHandle = setTimeout(function()
-			{
-				var time = now();
-				var currentRequests = requests;
-				var keys = getKeys(currentRequests);
-
-				// Reset data structure before executing callbacks
-				requests = {};
-				timeoutHandle = null;
-
-				// Process all callbacks
-				for (var i=0, l=keys.length; i<l; i++) {
-					currentRequests[keys[i]](time);
-				}
-			}, 1000 / TARGET_FPS);
-		}
-
-		return callbackHandle;
-	};
-
-	global.cancelRequestAnimationFrame = function(handle)
-	{
-		delete requests[handle];
-
-		// Stop timeout if all where removed
-		if (isEmpty(requests))
-		{
-			clearTimeout(timeoutHandle);
-			timeoutHandle = null;
-		}
-	};
-
+  // requestAnimationFrame polyfill
+  global.requestAnimationFrame = global.requestAnimationFrame
+      || global.webkitRequestAnimationFrame
+      || global.mozRequestAnimationFrame
+      || global.oRequestAnimationFrame
+      || global.msRequestAnimationFrame;
+  if(!global.requestAnimationFrame){
+    // Custom implementation
+    // Basic emulation of native methods for internal use
+    var now = Date.now || function() {
+      return new Date().getTime();
+    };
+    
+    var getKeys = Object.keys || function(obj) {
+      var keys = {};
+      for (var key in obj) {
+        keys[key] = true;
+      }
+      return keys;
+    };
+    
+    var isEmpty = Object.empty || function(obj) {
+      for (var key in obj) {
+        return false;
+      }
+      return true;
+    };
+    
+    //internal fields
+    var TARGET_FPS = 60;
+    var requests = {};
+    var rafHandle = 1;
+    var timeoutHandle = null;
+    //emulation
+    global.requestAnimationFrame=function(callback, root){
+      var callbackHandle = rafHandle++;
+      
+      // Store callback
+      requests[callbackHandle] = callback;
+      
+      // Create timeout at first request
+      if (timeoutHandle === null)
+      {
+        timeoutHandle = setTimeout(function()
+        {
+          var time = now();
+          var currentRequests = requests;
+          var keys = getKeys(currentRequests);
+          
+          // Reset data structure before executing callbacks
+          requests = {};
+          timeoutHandle = null;
+          
+          // Process all callbacks
+          for (var i=0, l=keys.length; i<l; i++) {
+            currentRequests[keys[i]](time);
+          }
+        }, 1000 / TARGET_FPS);
+      }
+      
+      return callbackHandle;
+    };
+    
+    global.cancelAnimationFrame=function(handle){
+      delete requests[handle];
+      
+      // Stop timeout if all where removed
+      if (isEmpty(requests))
+      {
+        clearTimeout(timeoutHandle);
+        timeoutHandle = null;
+      }
+    };
+  } else {
+    global.cancelAnimationFrame = global.cancelAnimationFrame
+        ||global.webkitCancelAnimationFrame
+        ||global.mozCancelAnimationFrame
+        ||global.oCancelAnimationFrame
+        ||global.msCancelAnimationFrame
+      //legacy support (cancelRequestAnimationFrame got renamed to cancelAnimationFrame)
+        ||global.cancelRequestAnimationFrame
+        ||global.webkitCancelRequestAnimationFrame
+        ||global.mozCancelRequestAnimationFrame
+        ||global.oCancelRequestAnimationFrame
+        ||global.msCancelRequestAnimationFrame
+  }
 })(this);
