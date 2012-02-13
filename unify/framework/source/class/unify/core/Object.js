c6959b7700f4559070ca286a6a28eba1772caa07
Sebastian Fastner <mail@sebastianfastner.de>
2012-02-13 10:45:15 +0100

    Generic base object

--- unify/framework/source/class/unify/core/Object.js
+++ unify/framework/source/class/unify/core/Object.js
@@ -4,29 +4,9 @@
  * @require {core.ext.Function}
  */
 core.Class("unify.core.Object", {
-  include : [core.property.MGeneric],
-  implement : [core.property.IEvent],
+  include : [lowland.Object],
   
   construct : function() {
-    this.__$$userdata = {};
-  },
-  
-  members : {
-    fireEvent : function(type, value, old) {
-    },
-    
-    fireDataEvent : function(type, value, old) {
-    },
-    
-    setUserData : function(key, value) {
-      this.__$$userdata[key] = value;
-    },
-    
-    getUserData : function(key) {
-      return this.__$$userdata[key];
-    },
-    
-    addListener : function(event, callback, context) {
-    }
+    lowland.Object.call(this);
   }
 });
