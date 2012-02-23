1b52596d7c52e0515ac4dd4c2c2453912a95a6c6
Sebastian Fastner <mail@sebastianfastner.de>
2012-02-23 16:55:02 +0100

    Initial implementing of VBox

--- unify/framework/source/class/unify/ui/layout/VBox.js
+++ unify/framework/source/class/unify/ui/layout/VBox.js
@@ -14,5 +14,68 @@
 core.Class("unify.ui.layout.VBox", {
   include : [unify.ui.layout.Base],
   
-  members : {}
+  properties : {
+    alignX: {
+      init: "left"
+    },
+    alignY: {
+      init: "top"
+    }
+  },
+  
+  members : {
+    __childrenCache : null,
+    
+    renderLayout : function(availWidth, availHeight) {
+      if (this._invalidChildrenCache) {
+        this.__rebuildChildrenCache();
+      }
+      
+      var top = 0;
+      
+      var cache = this.__childrenCache;
+      for (var i=0,ii=cache.length; i<ii; i++) {
+        var widget = cache[i];
+        var calc = widget.getLayoutProperties();
+        var size = widget.getSizeHint();
+        
+        var alignX = this.getAlignX();
+        var left;
+        var width;
+        var height = size.height;
+        
+        if (alignX == "left") {
+          left = 0;
+          width = size.width;
+        } else if (alignX == "center") {
+          left = Math.round(availHeight / 2 - size.height / 2);
+          width = size.width;
+        } else {
+          width = size.width;
+          left = availHeight - width;
+        }
+        
+        widget.renderLayout(left, top, width, height);
+        
+        top += height;
+      }
+    },
+    
+    _computeSizeHint : function() {
+      if (this._invalidChildrenCache) {
+        this.__rebuildChildrenCache();
+      }
+      
+      var cache = this.__childrenCache;
+      for (var i=0,ii=cache.length; i<ii; i++) {
+        var widget = cache[i];
+      }
+      
+      return null;
+    },
+    
+    __rebuildChildrenCache : function() {
+      this.__childrenCache = this._getLayoutChildren();
+    }
+  }
 });
