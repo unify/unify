cdf42c0ef1af69a6675bb6777cae04fef3f0336d
Sebastian Fastner <mail@sebastianfastner.de>
2012-02-28 18:50:15 +0100

    Refactor to use core and lowland

--- unify/framework/source/class/unify/bom/Transform.js
+++ unify/framework/source/class/unify/bom/Transform.js
@@ -11,58 +11,64 @@
 /**
  * Transform abstractor
  */
-core.Module("unify.bom.Transform", {
-  /**
-   * Generates translate string to have accelerated movement (if supported)
-   *
-   * @param x {Integer} X position of translate
-   * @param y {Integer} Y position of translate
-   * @return {String} Accelerated translate
-   */
-  accelTranslate : function(x, y) {
-    // Fix resize bug with translate3d on google chrome 15.0.874.121 m
-    if (qx.core.Environment.get("browser.name") == "chrome" || qx.core.Environment.get("engine.name") != "webkit") {
-      return "translate("+x+","+y+")";
-    } else {
-      return "translate3d("+x+","+y+",0)";
-    }
-  },
+(function() {
   
-  /**
-   * Generates translate string
-   *
-   * @param x {Integer} X position of translate
-   * @param y {Integer} Y position of translate
-   * @return {String} Translate
-   */
-  translate : function(x, y) {
-    return "translate("+x+","+y+")";
-  },
-
-  /**
-   * Generates scale string to have accelerated scaling (if supported)
-   *
-   * @param x {Integer} X factor of scale
-   * @param y {Integer} Y factor of scale
-   * @return {String} Accelerated scale
-   */
-  accelScale : function(x,y){
-    // Fix resize bug with translate3d on google chrome 15.0.874.121 m
-    if (qx.core.Environment.get("browser.name") == "chrome" || qx.core.Environment.get("engine.name") != "webkit") {
+  var chrome = (navigator.userAgent.toLowerCase().indexOf("chrome") >= 0);
+  
+  core.Module("unify.bom.Transform", {
+    /**
+     * Generates translate string to have accelerated movement (if supported)
+     *
+     * @param x {Integer} X position of translate
+     * @param y {Integer} Y position of translate
+     * @return {String} Accelerated translate
+     */
+    accelTranslate : function(x, y) {
+      // Fix resize bug with translate3d on google chrome 15.0.874.121 m
+      if (chrome || core.Env.getValue("engine") != "webkit") {
+        return "translate("+x+","+y+")";
+      } else {
+        return "translate3d("+x+","+y+",0)";
+      }
+    },
+    
+    /**
+     * Generates translate string
+     *
+     * @param x {Integer} X position of translate
+     * @param y {Integer} Y position of translate
+     * @return {String} Translate
+     */
+    translate : function(x, y) {
+      return "translate("+x+","+y+")";
+    },
+  
+    /**
+     * Generates scale string to have accelerated scaling (if supported)
+     *
+     * @param x {Integer} X factor of scale
+     * @param y {Integer} Y factor of scale
+     * @return {String} Accelerated scale
+     */
+    accelScale : function(x,y){
+      // Fix resize bug with translate3d on google chrome 15.0.874.121 m
+      if (chrome || core.Env.getValue("engine") != "webkit") {
+        return "scale("+x+","+y+")";
+      } else {
+        return "scale3d("+x+","+y+",1)";
+      }
+    },
+  
+    /**
+     * Generates scale string
+     *
+     * @param x {Integer} X factor of scale
+     * @param y {Integer} Y factor of scale
+     * @return {String} Scale
+     */
+    scale : function (x,y){
       return "scale("+x+","+y+")";
-    } else {
-      return "scale3d("+x+","+y+",1)";
     }
-  },
+  });
 
-  /**
-   * Generates scale string
-   *
-   * @param x {Integer} X factor of scale
-   * @param y {Integer} Y factor of scale
-   * @return {String} Scale
-   */
-  scale : function (x,y){
-    return "scale("+x+","+y+")";
-  }
-});
+})();
