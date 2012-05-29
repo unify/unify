fd2fcb49c06da5441e06de644e8bca981db45834
Sebastian Fastner <mail@sebastianfastner.de>
2012-05-29 15:49:40 +0200

    Fix widget positioning on firefox

--- unify/framework/source/class/unify/ui/core/Util.js
+++ unify/framework/source/class/unify/ui/core/Util.js
@@ -1,30 +1,6 @@
 core.Module("unify.ui.core.Util", {
   getWidgetLocation : function(widget) {
-    var origPos = lowland.bom.Element.getLocation(widget.getElement());
-    var pos = {
-      left: origPos.left,
-      top: origPos.top
-    };
-    
-    // Firefox don't calculate with transforms! So we have to calculate this and add it to position.
-    if (core.Env.getValue("engine") == "gecko") {
-      var parent = widget;
-      var left = 0;
-      var top = 0;
-
-      while ((parent = parent.getParentBox())) {
-        var transform = core.bom.Style.get(parent.getElement(), "transform").split(",");
-        if (transform && transform[0] != "none") {
-          left += parseFloat(transform[4]);
-          top += parseFloat(transform[5]);
-        }
-      }
-      
-      pos.left += left;
-      pos.top += top;
-    }
-    
-    return pos;
+    return lowland.bom.Element.getLocation(widget.getElement());
   },
   
   domElementToRootLevel : function(widget) {
