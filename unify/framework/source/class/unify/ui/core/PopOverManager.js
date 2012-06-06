796a27df09d2c17a52996525fa505a68599ddd72
Dominik Göpel <dominik.goepel@gmx.de>
2012-06-06 14:06:19 +0200

    fix in PopOverManager destructor: don't remove blocker elements from root, root is already gone at this point

--- unify/framework/source/class/unify/ui/core/PopOverManager.js
+++ unify/framework/source/class/unify/ui/core/PopOverManager.js
@@ -233,10 +233,7 @@ qx.Class.define("unify.ui.core.PopOverManager",
     
   destruct : function() {
     qx.event.Registration.removeListener(this.__pblocker, 'tap', this.__onTapBlocker, this);
-    
-    var rootElement = this.__root.getElement();
-    rootElement.removeChild(this.__pblocker);
-    rootElement.removeChild(this.__mblocker);
+
     this.__root = this.__pblocker = this.__mblocker = this.__overlays = this.__visibleOverlays = this.__styleRegistry = null;
   } 
 });
