f5ce536bb12a1ee9aaf8710d5a09dd40d20313d5
Sebastian Fastner <mail@sebastianfastner.de>
2012-06-18 11:12:35 +0200

    Add check for DOM ready in init script

--- unify/framework/source/class/unify/core/Init.js
+++ unify/framework/source/class/unify/core/Init.js
@@ -10,17 +10,31 @@
     lowland.ObjectManager.dispose();
   };
   
+  var DOMReadyEvent = (core.Env.getValue("engine") == "trident") ? "readystatechange" : "DOMContentLoaded";
+  
+  var runStartup = function() {
+    if (document.readyState === "interactive" || document.readyState === "complete") {
+      lowland.bom.Events.unlisten(document, DOMReadyEvent, runStartup);
+      
+      var Application = core.Class.getByName(core.Env.getValue("application") + ".Application");
+      var init = new Application();
+      
+      app = init;
+  
+      init.main();
+      init.finalize();
+      
+      //lowland.bom.Events.listen(window, "shutdown", shutDown);
+      //lowland.bom.Events.listen(window, "beforeunload", shutDown);
+    }
+  };
+  
   var startUp = function() {
-    var Application = core.Class.getByName(core.Env.getValue("application") + ".Application");
-    var init = new Application();
-    
-    app = init;
-
-    init.main();
-    init.finalize();
-    
-    //lowland.bom.Events.listen(window, "shutdown", shutDown);
-    //lowland.bom.Events.listen(window, "beforeunload", shutDown);
+    if (document.readyState === "interactive" || document.readyState === "complete") {
+      runStartup();
+    } else {
+      lowland.bom.Events.listen(document, DOMReadyEvent, runStartup);
+    }
   };
   
 
