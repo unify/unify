fdb77013cc4b6eb7c0ea5c3987caaf6c4ebb5994
Stefan Kolb <stefan.kolb@indiginox.com>
2012-08-02 11:24:08 +0200

    Made changes to initialization of application so it would work in IE (again); Some code clean-up; Added some basic comments

--- unify/framework/source/class/unify/core/Init.js
+++ unify/framework/source/class/unify/core/Init.js
@@ -1,71 +1,106 @@
 (function() {
 
   var app;
-  
+  var DOMReadyEvent;
+
+  // Determine the proper event for DOM is ready
+  if (core.Env.getValue('engine') == 'trident') {
+    DOMReadyEvent = 'readystatechange';
+  } else {
+    DOMReadyEvent = 'DOMContentLoaded';
+  }
+
+
+  /**
+   * Is document ready?
+   *
+   * @return {Boolean} true: Document/DOM is ready;
+   *                   false: otherwise
+   */
+  var isDocReady = function() {
+    if (document.readyState === 'interactive' || document.readyState === 'complete') {
+      return true;
+    }
+
+    return false;
+  };
+
+
+  /**
+   * Handle document state changes
+   *
+   * Called when the state of the document changes, for example from
+   * interactive to complete.
+   *
+   * @param event {Event} The state change event
+   */
+  var onDocStateChange = function(event) {
+    if (isDocReady()) {
+      runStartUp();
+    }
+  };
+
+
+  /**
+   * Run start routine
+   *
+   * When the document is ready, the application is initialized.
+   */
+  var runStartUp = function() {
+    if (isDocReady()) {
+      lowland.bom.Events.unlisten(document, DOMReadyEvent, onDocStateChange);
+
+      var Application = core.Class.getByName(core.Env.getValue('application') + '.Application');
+      var init = app = new Application();
+
+      init.main();
+      init.finalize();
+    }
+  };
+
+
+  /**
+   * Get application
+   *
+   * Returns reference to the (instantiated) application
+   *
+   * @return {var} Reference to the (instantiated) application
+   */
   var getApplication = function() {
     return app;
   };
-  
+
+
+  /**
+   * Start-up
+   */
+  var startUp = function() {
+    if (isDocReady()) {
+      runStartUp();
+    } else {
+      lowland.bom.Events.listen(document, DOMReadyEvent, onDocStateChange);
+    }
+  };
+
+
+  /**
+   * Shut-down
+   */
   var shutDown = function() {
     lowland.ObjectManager.dispose();
   };
-  
-  var DOMReadyEvent;
-  var DOMReadyState;
-  
-  if (core.Env.getValue("engine") == "trident") {
-    DOMReadyEvent = "readystatechange";
-  } else {
-    DOMReadyEvent = "DOMContentLoaded";
-  }
-  
-  var runStartup = function() {
-    if (document.readyState === "interactive" || document.readyState === "complete") {
-      lowland.bom.Events.unlisten(document, DOMReadyEvent, runStartup);
-      
-      var Application = core.Class.getByName(core.Env.getValue("application") + ".Application");
-      var init = new Application();
-      
-      app = init;
-  
-      init.main();
-      init.finalize();
-      
-      //lowland.bom.Events.listen(window, "shutdown", shutDown);
-      //lowland.bom.Events.listen(window, "beforeunload", shutDown);
-    }
-  };
-  
-  var readyStateChange = function() {
-    if (core.Env.getValue("engine") == "trident") {
-      if (document.readyState === "complete") {
-        runStartup();
-      }
-    } else {
-      runStartup();
-    }
-  };
-  
-  var startUp = function() {
-    if (core.Env.getValue("engine") == "trident") {
-      lowland.bom.Events.listen(document, "readystatechange", readyStateChange);
-    } else {
-      if (document.readyState === "interactive" || document.readyState === "complete") {
-        runStartup();
-      } else {
-        lowland.bom.Events.listen(document, "DOMContentLoaded", readyStateChange);
-      }
-    }
-  };
-  
 
+
+  /**
+   * Init
+   */
   core.Class("unify.core.Init", {
     include : [unify.core.Object],
-    
+
     construct : function() {
       unify.core.Object.call(this);
     },
-    
+
     members : {
       main : function() {
         throw new Error("main is not implemented");
@@ -73,7 +108,7 @@
       finalize : function() {}
     }
   });
-  
+
   unify.core.Statics.annotate(unify.core.Init, {
     getApplication : getApplication,
     startUp : startUp,
@@ -81,3 +116,4 @@
   });
 
 })();
+
