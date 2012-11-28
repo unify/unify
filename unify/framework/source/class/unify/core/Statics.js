73772bc36de57b873c8e1d4f94bce7411e37b835
Sebastian Fastner <mail@sebastianfastner.de>
2012-11-28 10:40:02 +0100

    Fix API generation run of static part

--- unify/framework/source/class/unify/core/Statics.js
+++ unify/framework/source/class/unify/core/Statics.js
@@ -1,7 +1,13 @@
-core.Module("unify.core.Statics", {
-	annotate: function(Clazz, statics) {
-		var clsname = (""+Clazz).substring(7);
-		clsname = clsname.substring(0, clsname.length-1);
-		core.Main.addStatics(clsname, statics);
-	}
-});
+(function() {
+
+	var addStatics = core.Main.addStatics;
+
+	core.Module("unify.core.Statics", {
+		annotate: function(Clazz, statics) {
+			var clsname = (""+Clazz).substring(7);
+			clsname = clsname.substring(0, clsname.length-1);
+			addStatics(clsname, statics);
+		}
+	});
+
+})();
