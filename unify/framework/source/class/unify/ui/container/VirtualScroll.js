cbc4eaa5c7a226f854f48d042229861162a06f53
Sebastian Fastner <mail@sebastianfastner.de>
2013-01-11 15:56:59 +0100

    Add x and y offset

--- unify/framework/source/class/unify/ui/container/VirtualScroll.js
+++ unify/framework/source/class/unify/ui/container/VirtualScroll.js
@@ -1,8 +1,8 @@
 core.Class("unify.ui.container.VirtualScroll", {
 	include : [unify.ui.container.Scroll],
 	
-	construct : function(elementWidth, elementHeight, widgetFactory, widgetUpdater, layout) {
-		unify.ui.container.Scroll.call(this, layout);
+	construct : function(elementWidth, elementHeight, widgetFactory, widgetUpdater) {
+		unify.ui.container.Scroll.call(this, new unify.ui.layout.Basic());
 		
 		this.addListener("scroll", this.__onVirtualScroll, this);
 		this.__elementHeight = elementHeight;
@@ -23,13 +23,26 @@ core.Class("unify.ui.container.VirtualScroll", {
 		model : {
 			init: null,
 			nullable: true,
-			apply: function(value) { this.__applyModel(value); }
+			apply: function(value) { this.__applyModel(value); },
+			fire: "changeModel"
 		},
 		
 		columns : {
 			init: 1,
 			type: "integer"
+		},
+		
+		offsetX : {
+			init: 0
+		},
+		
+		offsetY : {
+			init: 0
 		}
+	},
+	
+	events : {
+		changeModel: lowland.events.Event
 	},
 	
 	members : {
@@ -86,33 +99,36 @@ core.Class("unify.ui.container.VirtualScroll", {
 				}
 			}
 			
+			var offsetX = this.getOffsetX();
+			var offsetY = this.getOffsetY();
+			
 			// If new rows appears that have no rendered widgets, do it
 			for (var row = startRow; row <= endRow; row++) {
 				var columns = this.getColumns();
 				for (var i=0; i<columns; i++) {
 					if ((!widgetMap[row]) || (!widgetMap[row][i])) {
-						this.__createWidget(i, row);
+						this.__createWidget(i, row, offsetX, offsetY);
 					}
 				}
 			}
 			
 		},
 		
-		__createWidget : function(column, row) {
+		__createWidget : function(column, row, offsetX, offsetY) {
 			var widgetUpdater = this.__widgetUpdater;
 			var widget = this.__getWidgetFromPool();
 			
 			widgetUpdater(widget, column, row, false);
 			this.add(widget);
 			
-			widget.overrideLayoutTransform(column*this.__elementWidth, row*this.__elementHeight);
+			widget.overrideLayoutTransform(column*this.__elementWidth+offsetX, row*this.__elementHeight+offsetY);
 			
 			if (!this.__widgetMap[row]) {
 				this.__widgetMap[row] = {};
 			}
 			this.__widgetMap[row][column] = widget;
 			
-			var pos = column * this.getColumns() + row;
+			var pos = row * this.getColumns() + column;
 			widgetUpdater.lazy(this, widget, column, row, this.getModel()[pos]);
 		},
 		
