4819aa98166b943eb7f36b170c141ca8b247d579
Dominik Göpel <dominik.goepel@gmx.de>
2013-07-09 10:58:22 +0200

    only relayout label on value change if it has autoCalculateSize true

--- unify/framework/source/class/unify/ui/basic/Label.js
+++ unify/framework/source/class/unify/ui/basic/Label.js
@@ -201,7 +201,9 @@ qx.Class.define("unify.ui.basic.Label", {
      */
     _applyValue : function(value) {
       this.__contentSize = null;
-      this.invalidateLayoutChildren();
+      if(this.getAutoCalculateSize()){
+        this.invalidateLayoutChildren();
+      }
       qx.bom.Label.setValue(this.getElement(), value);
     },
 
